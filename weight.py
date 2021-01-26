18d2b720d76f676552d12ff095f0f53a1720e550
SarahDoyle-96 <78006601+SarahDoyle-96@users.noreply.github.com>
2021-01-25 20:39:57 -0700

    Add files via upload

---
+++ weight.py
@@ -0,0 +1,9 @@
+# weightconvert.py
+# A program to convert kiligrams to pounds
+# Written by Sarah Doyle
+
+def main():
+    kilograms = eval(input("What is the weight in kilograms? "))
+    pounds = 2.2046226218 * kilograms
+    print("The weight is", pounds, "pounds.")
+main()
